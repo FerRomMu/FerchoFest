4708bcf038664f5f7daa2d1c01ff4a9b749d6540
Fernando Romero <fer.rom.mu@gmail.com>
2022-05-12 23:56:46 -0300

    Se modifico los cards. Se agrego animacion.

--- script/window.js
+++ script/window.js
@@ -2,12 +2,15 @@ buttonInfo = document.querySelector("#infoB");
 buttonFaq = document.querySelector("#faqB");
 info = document.querySelector("#info");
 faq = document.querySelector("#faq");
+w = document.querySelector(".window");
 
 function show(card) {
   card.classList.remove('hidden');
+  w.classList.add('show');
 }
 
 function hideMenu(card) {
+  w.classList.remove('show');
   card.classList.add('hidden');
 }
 
